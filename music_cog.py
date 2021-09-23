07040f87eaf1afad9d120bfa7d7a3df5af7e7101
le-stab <68471427+le-stab@users.noreply.github.com>
2021-09-22 21:51:36 -0300

    massive upgrade

--- music_cog.py
+++ music_cog.py
@@ -8,6 +8,7 @@ from discord import channel
 from discord import voice_client
 from discord.voice_client import VoiceClient
 import ffmpeg
+import re
 
 class music_cog(commands.Cog):
     def __init__(self, bot):
@@ -22,12 +23,21 @@ class music_cog(commands.Cog):
         self.vc = ""
 
     def search_yt(self, item):
-        with YoutubeDL(self.YDL_OPTIONS) as ydl:
-            try:
-                info = ydl.extract_info("ytsearch:%s" % item, download=False)['entries'][0]
-            except Exception:
-                return False
-        return {'source' : info['formats'][0]['url'], 'title': info['title']}
+        global info
+        urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*(),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', item)
+        if urls:
+            with YoutubeDL(self.YDL_OPTIONS) as ydl:
+                info = ydl.extract_info(item, download=False)
+                url2 = info['formats'][0]['url']
+                discord.FFmpegOpusAudio.from_probe(url2, **self.FFMPEG_OPTIONS)
+            return {'source' : info['formats'][0]['url'], 'title': info['title']}
+        else:
+            with YoutubeDL(self.YDL_OPTIONS) as ydl:
+                try:
+                    info = ydl.extract_info("ytsearch:%s" % item, download=False)['entries'][0]
+                except Exception:
+                    return False
+            return {'source' : info['formats'][0]['url'], 'title': info['title']}
 
     def play_next(self):
         if len(self.music_queue) > 0:
@@ -35,7 +45,7 @@ class music_cog(commands.Cog):
 
             m_url = self.music_queue[0][0]['source']
 
-            self.music_queue.pop[0]
+            self.music_queue.pop(0)
 
             self.vc.play(discord.FFmpegPCMAudio(m_url, **self.FFMPEG_OPTIONS), after=lambda e: self.play_next())
         else:
@@ -52,9 +62,7 @@ class music_cog(commands.Cog):
             else:
                 pass
 
-            print(self.music_queue)
             self.music_queue.pop(0)
-
             self.vc.play(discord.FFmpegPCMAudio(m_url, **self.FFMPEG_OPTIONS), after = lambda e: self.play_next())
         else:
             self.is_playing = False
@@ -62,37 +70,117 @@ class music_cog(commands.Cog):
     @commands.command(aliases=['p'])
     async def play(self, ctx, *args):
         query = " ".join(args)
+        user = ctx.message.author
 
-        voice_channel = ctx.message.author.voice.channel
-        if voice_channel is None:
-            await ctx.send('You need to connect to a voice channel to play music')
+        vc_ch_mem = ctx.message.author.voice
+        if vc_ch_mem is None:
+            song_embed = discord.Embed(
+                  title = f"{user.name}, there was a problem",
+                  description = f"To play music, you need to first connect to a voice channel",
+                  color = discord.Color.from_rgb(232,14,51)
+              )
+            song_embed.set_footer(text='🎵 Play')
+            await ctx.reply(embed=song_embed, mention_author = False)
+            await ctx.message.add_reaction('🎵')
         else:
             song = self.search_yt(query)
             if type(song) == type(True):
-                await ctx.send('Could not download song. Incorrect format, try another keyword')
+                song_embed = discord.Embed(
+                  title = f"{user.name}, there was a problem",
+                  description = f"That song was either not found, or currently not available\nPlease try again",
+                  color = discord.Color.from_rgb(232,14,51)
+                )
+                song_embed.set_footer(text='🎵 Play')
+                await ctx.reply(embed=song_embed, mention_author = False)
+                await ctx.message.add_reaction('🎵')
             else:
-                await ctx.send('Song added to the queue')
+                voice_channel = ctx.message.author.voice.channel
                 self.music_queue.append([song, voice_channel])
 
                 if self.is_playing == False:
                     await self.play_music()
 
+                if len(self.music_queue) == 0:
+                    song_embed = discord.Embed(
+                        title = f"{user.name} | Playing song",
+                        description = f"Playing **`{info['title']}`**",
+                        color = discord.Color.from_rgb(13,217,199)
+                    )
+                    song_embed.set_footer(text='🎵 Play')
+                    await ctx.reply(embed=song_embed, mention_author = False)
+                    await ctx.message.add_reaction('🎵')
+                else:
+                    song_embed = discord.Embed(
+                        title = f"{user.name} | Added to queue",
+                        description = f"Added **`{info['title']}`** to queue",
+                        color = discord.Color.from_rgb(109,167,250)
+                    )
+                    song_embed.set_footer(text='🎵 Play')
+                    await ctx.reply(embed=song_embed, mention_author = False)
+                    await ctx.message.add_reaction('🎵')
+
     @commands.command(aliases=['q'])
     async def queue(self, ctx):
+        user = ctx.message.author
         retval = ""
 
         for i in range(0, len(self.music_queue)):
-            retval += self.music_queue[i][0]['title'] + "\n"
+            if i != len(self.music_queue):
+                retval += "**`" + self.music_queue[i][0]['title'] + "`**\n—\n"
 
-        print(retval)
         if retval != "":
-            await ctx.send(retval)
+            song_embed = discord.Embed(
+                        title = f"{user.name} | Queue",
+                        description = f"*`{i+1}` songs in queue*\n\n—\n{retval}",
+                        color = discord.Color.from_rgb(109,167,250)
+            )
+            song_embed.set_footer(text='📃 Queue')
+            await ctx.reply(embed=song_embed, mention_author = False)
+            await ctx.message.add_reaction('📃')
         else:
-            await ctx.send('No music in queue')
+            song_embed = discord.Embed(
+                  title = f"{user.name}, queue is empty",
+                  description = f"There is no music queued up",
+                  color = discord.Color.from_rgb(232,14,51)
+            )
+            song_embed.set_footer(text='📃 Queue')
+            await ctx.reply(embed=song_embed, mention_author = False)
+            await ctx.message.add_reaction('📃')
+
+    @commands.command(aliases=['st'])
+    async def stop(self, ctx):
+        user = ctx.message.author
+        if self.vc and self.vc.is_playing():
+            song_embed = discord.Embed(
+                  title = f"{user.name} | Music stopped",
+                  description = f"Music has been stopped",
+                  color = discord.Color.from_rgb(222,46,44)
+            )
+            song_embed.set_footer(text='🛑 Stop')
+            await ctx.reply(embed=song_embed, mention_author = False)
+            await ctx.message.add_reaction('🛑')
+            self.vc.stop()
+        else:
+            song_embed = discord.Embed(
+                  title = f"{user.name}, no music is playing",
+                  description = f"There is no music playing",
+                  color = discord.Color.from_rgb(232,14,51)
+            )
+            song_embed.set_footer(text='🛑 Stop')
+            await ctx.reply(embed=song_embed, mention_author = False)
+            await ctx.message.add_reaction('🛑')
 
     @commands.command(aliases=['s'])
     async def skip(self, ctx):
+        user = ctx.message.author
         if self.vc != "":
             self.vc.stop()
             await self.play_music()
-            await ctx.send('Skipped!')
+            song_embed = discord.Embed(
+                  title = f"{user.name} | Skipped",
+                  description = f"Skipped current song (even if there are not any other songs in queue)",
+                  color = discord.Color.from_rgb(213,234,247)
+            )
+            song_embed.set_footer(text='⏩ Skip')
+            await ctx.reply(embed=song_embed, mention_author = False)
+            await ctx.message.add_reaction('⏩')
--- sn_core.py
+++ sn_core.py
@@ -19,4 +19,57 @@ async def on_ready():
     print('Sonus Ready')
     await client.change_presence(activity=discord.Streaming(name='Music', url='https://www.twitch.tv/tehllamah'))
 
+@client.command()
+async def help(ctx, cata=None):
+    user = ctx.message.author
+    if cata == None:
+        song_embed = discord.Embed(
+                  title = f"{user.name} | Help (All commands)",
+                  description = f"`play`,`queue`,`stop`,`skip`",
+                  color = discord.Color.from_rgb(182,224,222)
+              )
+        song_embed.set_footer(text='🔧 Help | To see how a command works, type: help <name of command>')
+        await ctx.reply(embed=song_embed, mention_author = False)
+        await ctx.message.add_reaction('🔧')
+    
+    elif cata == 'play':
+        song_embed = discord.Embed(
+                  title = f"{user.name} | Play",
+                  description = f"**`Play`**\n\nSyntax: `s.play <query (Youtube URL or word search)>`\n\nPlays music, you can either insert a **Youtube** URL, or just type out your query, like this:\n\n`s.play Classical Music` | Plays the closest search for Classical Music",
+                  color = discord.Color.from_rgb(182,224,222)
+              )
+        song_embed.set_footer(text='🔧 Help | Aliases : p')
+        await ctx.reply(embed=song_embed, mention_author = False)
+        await ctx.message.add_reaction('🔧')
+
+    elif cata == 'queue':
+        song_embed = discord.Embed(
+                  title = f"{user.name} | Queue",
+                  description = f"**`Play`**\n\nSyntax: `s.queue`\n\nShows music queue, if there is any in the first place",
+                  color = discord.Color.from_rgb(182,224,222)
+              )
+        song_embed.set_footer(text='🔧 Help | Aliases : q')
+        await ctx.reply(embed=song_embed, mention_author = False)
+        await ctx.message.add_reaction('🔧')
+
+    elif cata == 'stop':
+        song_embed = discord.Embed(
+                  title = f"{user.name} | Stop",
+                  description = f"**`Play`**\n\nSyntax: `s.stop`\n\nStops all music reproduction",
+                  color = discord.Color.from_rgb(182,224,222)
+              )
+        song_embed.set_footer(text='🔧 Help | Aliases : st')
+        await ctx.reply(embed=song_embed, mention_author = False)
+        await ctx.message.add_reaction('🔧')
+
+    elif cata == 'skip':
+        song_embed = discord.Embed(
+                  title = f"{user.name} | Skip",
+                  description = f"**`Play`**\n\nSyntax: `s.skip`\n\nSkips current song, it will then play the next song in the queue, if there is no other song, it will play nothing",
+                  color = discord.Color.from_rgb(182,224,222)
+              )
+        song_embed.set_footer(text='🔧 Help | Aliases : s')
+        await ctx.reply(embed=song_embed, mention_author = False)
+        await ctx.message.add_reaction('🔧')
+
 client.run(TOKEN)
